035351541deef33f0bad689dcd65facf03aa2599
FTE <2336812+felipetesc@users.noreply.github.com>
2023-03-14 21:33:39 -0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+use tk::*;
+use tk::cmd::*;
+
+fn main() -> TkResult<()> {
+    let tk = make_tk!()?;
+    let root = tk.root();
+    root.add_label( -text("hello,world!") )?.pack(())?;
+    Ok( main_loop() )
+}
